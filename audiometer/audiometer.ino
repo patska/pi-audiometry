cf69f172bd6797924f90a700f67a6e043fe5e6dd
Lenon Cristhians <lenoncristhians@hotmail.com>
2020-11-16 10:05:44 -0300

    Status LEDs created

--- audiometer/audiometer.ino
+++ audiometer/audiometer.ino
@@ -1,37 +1,78 @@
+const int STOP_BUTTON = 2;
+const int START_BUTTON = 3;
+const int BLUE_LED = 9;
+const int GREEN_LED = 10;
+const int RED_LED = 11;
 
-const int START_BUTTON_PIN = 2;     
-const int START_EAR_SIDE_PIN = 3; 
-const int INPUT_BUTTON = 6:
-const int LED_START = 4; 
-const int LED_STOP = 5;
-int startButtonState = 0;  
-int earSideButtonState = 0;
+#define COMMON_ANODE
 
-void setup() {
-  pinMode(START_BUTTON_PIN, INPUT);
-  pinMode(START_EAR_SIDE_PIN, INPUT);
-  pinMode(INPUT_BUTTON, INPUT);
-  pinMode(LED_START, OUTPUT);
-  pinMode(LED_STOP, OUTPUT);
+int stopButtonState = 0;
+int startButtonState = 0;
+int exitCode = 0;
 
-  Serial.begin(9600);
+void setColor(int red, int green, int blue)
+{
+
+#ifdef COMMON_ANODE
+  red = 255 - red;
+  green = 255 - green;
+  blue = 255 - blue;
+#endif
+  analogWrite(RED_LED, red);
+  analogWrite(GREEN_LED, green);
+  analogWrite(BLUE_LED, blue);
 }
 
-void loop() {
-  tone(2200, 6);
-  startButtonState = digitalRead(START_BUTTON_PIN);
-  earSideButtonState = digitalRead(START_EAR_SIDE_PIN);
-  if (startButtonState == 1) {
-    Serial.println("Button ON");
-    digitalWrite(LED_START, 1);
-    digitalWrite(LED_STOP, 0);
-    if (earSideButtonState == 0) Serial.println("Inicio pela orelha direita!");
-    else Serial.println("Inicio pela orelha esquerda!");
-  } else {
-    Serial.println("Button OFF");
-    digitalWrite(LED_START, LOW);
-    digitalWrite(LED_STOP, HIGH);
+void turnOnRedLight()
+{
+  setColor(255, 0, 0);
+}
+
+void turnOnGreenLight()
+{
+  setColor(0, 255, 0);
+}
+
+void turnOnBlueLight()
+{
+  setColor(0, 0, 255);
+}
+
+void startAudiometerTest(){
+  while (exitCode == 0)
+  {
+    turnOnBlueLight();
+    chooseMainEarSide();
+    exitChecker();
   }
+}
+
+void exitChecker(){
+  stopButtonState = digitalRead(STOP_BUTTON);
+  if (stopButtonState  == 1){
+    exitCode = 1;
+    startButtonState = 0;
+  };
+}
+
+void chooseMainEarSide(){
   
-  delay(2000);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(START_BUTTON, INPUT);
+  pinMode(STOP_BUTTON, INPUT);
+}
+
+void loop()
+{
+  exitCode = 0;
+  turnOnRedLight();
+  startButtonState = digitalRead(START_BUTTON);
+  if(startButtonState == 1) startAudiometerTest();
 }
