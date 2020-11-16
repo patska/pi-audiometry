98284bb9737472d9884d0337c562b1ad2f91f8b4
Lenon Cristhians <lenoncristhians@hotmail.com>
2020-11-16 14:55:39 -0300

    Ear test structure created

--- audiometer/audiometer.ino
+++ audiometer/audiometer.ino
@@ -1,6 +1,9 @@
+#include <Volume.h>
+
 const int STOP_BUTTON = 2;
 const int START_BUTTON = 3;
-const int ACTION_BUTTON = 4;
+const int RIGHT_BUTTON = 4;
+const int LEFT_BUTTON = 7;
 const int BLUE_LED = 9;
 const int GREEN_LED = 10;
 const int RED_LED = 11;
@@ -9,8 +12,10 @@ const int RED_LED = 11;
 
 int stopButtonState = 0;
 int startButtonState = 0;
-int actionButtonState = 0;
+int rightButtonState = 0;
+int leftButtonState = 0;
 int exitCode = 0;
+Volume vol;
 
 void setColor(int red, int green, int blue)
 {
@@ -40,30 +45,64 @@ void turnOnBlueLight()
   setColor(0, 0, 255);
 }
 
-void startAudiometerTest(){
+void turnOnOrangeLight()
+{
+  setColor(255, 99, 71);
+}
+
+void startAudiometerTest()
+{
   while (exitCode == 0)
   {
     turnOnBlueLight();
-    chooseMainEarSide();
+    int earSide = chooseMainEarSide();
     exitChecker();
   }
 }
 
-void exitChecker(){
+void exitChecker()
+{
   stopButtonState = digitalRead(STOP_BUTTON);
-  if (stopButtonState  == 1){
+  if (stopButtonState == 1)
+  {
     exitCode = 1;
     startButtonState = 0;
+    vol.end();
   };
 }
 
-void chooseMainEarSide(){
+int chooseMainEarSide()
+{
   while (exitCode == 0)
   {
+    //testLeftEar();
+    // testRightEar();
+
+    while (exitCode == 0)
+    {
+      exitChecker();
+    }
 
     exitChecker();
   }
-  
+
+  return 0;
+}
+
+void testLeftEar()
+{
+  vol.alternatePin(false);
+  vol.tone(1000, 255);
+  vol.delay(1000);
+  vol.end();
+}
+
+void testRightEar()
+{
+  vol.alternatePin(true);
+  vol.tone(1000, 255);
+  vol.delay(1000);
+   vol.end();
 }
 
 void setup()
@@ -74,6 +113,7 @@ void setup()
   pinMode(RED_LED, OUTPUT);
   pinMode(START_BUTTON, INPUT);
   pinMode(STOP_BUTTON, INPUT);
+  vol.begin();
 }
 
 void loop()
@@ -81,5 +121,6 @@ void loop()
   exitCode = 0;
   turnOnRedLight();
   startButtonState = digitalRead(START_BUTTON);
-  if(startButtonState == 1) startAudiometerTest();
+  if (startButtonState == 1)
+    startAudiometerTest();
 }
